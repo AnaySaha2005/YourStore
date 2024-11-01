9fbe50b0f02c887207a07bfb7d50331ae5d9a00e
Anay Saha <anaysaha2005@gmail.com>
2024-11-01 13:36:24 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const isAuthorized = require("./MIddlewares/is Authorized");
 const Retailer = require("./models/retailer");
 const Items = require("./models/items");
 const corsOptions = {
-  origin: "https://your-store-alpha.vercel.app",
+  origin: "https://your-store-alpha.vercel.app/",
   credentials: true,
 };
 const multer = require("multer");
