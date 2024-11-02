cca9adb22bf9c3591f77da9a1bfdcf5fb4ed1254
Anay Saha <anaysaha2005@gmail.com>
2024-11-02 10:20:23 +0530

    .

--- backend/cloudConfig.js
+++ backend/cloudConfig.js
@@ -1,8 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-console.log("here"+process.env.ClOUD_NAME)
+console.log("here"+process.env.CLOUD_NAME)
 cloudinary.config({
-  cloud_name: process.env.ClOUD_NAME,
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret:process.env.CLOUD_API_SECRET
 });
